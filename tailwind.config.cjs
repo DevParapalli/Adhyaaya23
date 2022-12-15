78c83ee2dcd87eccaa947eb7e5563cece969e9e9
Devansh Parapalli <parapallidev@gmail.com>
2022-12-15 16:23:30 +0530

    added theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,23 @@ const config = {
 	theme: {
 		extend: {}
 	},
-
+	daisyui: {
+		themes: [
+			{
+				adhyaaya23: {
+					"primary": "#ffffff",
+					"secondary": "#ffd700",
+					"accent": "#222A51",
+					"neutral": "#191D24",
+					"base-100": "#000000",
+					"info": "#3ABFF8",
+					"success": "#36D399",
+					"warning": "#f2b8b5",
+					"error": "#b32613",
+				},
+			},
+		],
+	},
 	plugins: [forms, typography, daisyui]
 };
 
