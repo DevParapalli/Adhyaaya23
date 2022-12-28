d32308616a36b92077df17e87c9f09e9bcc5e24f
Devansh Parapalli <parapallidev@gmail.com>
2022-12-29 01:16:05 +0530

    structure of registration form ???

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -6,8 +6,8 @@ export interface AdhyaayaEventContact {
 export interface AdhyaayaEvent {
     // event reference id, used in the url and internally
     id: string;
-    mode: string; // Offline / Online etc
-    category: string; // Technical / Non-Technical etc
+    mode: 'offline' | 'online'; // Offline / Online etc
+    category: 'technical' | 'non-technical' | 'workshops'; // Technical / Non-Technical etc
     // Title of the event, used everywhere publicly
     name: string;
     description: string;
@@ -15,7 +15,7 @@ export interface AdhyaayaEvent {
     amount: number[];
     team_members: number[];
     // Gives the date and time of the event
-    start_date: Date;
+    start_date: Date; 
     end_date: Date;
 
     small_image: string; // Image for the card
@@ -25,4 +25,26 @@ export interface AdhyaayaEvent {
     is_active: boolean; // set this to false to hide the event from the list
 }
 
-export const EVENTS: AdhyaayaEvent[] = []
+export const EVENTS: AdhyaayaEvent[] = [
+    {
+        id: 'avishkar',
+        mode: 'offline',
+        category: 'technical',
+        name: 'Avishkar',
+        description: 'blah blah blah', //Avishkar is a technical event that tests your knowledge in the field of computer science and technology. It is a 24-hour event that will test your skills in programming, debugging, and logical thinking. It is a team event with a maximum of 3 members per team. The event will be held on 27th and 28th February 2021.
+        amount: [160, 160, 160, 160, 160],
+        team_members: [1, 2, 3, 4, 5],
+        start_date: new Date('28 Feb 2023'),
+        end_date: new Date('28 Feb 2023'),
+        small_image: 'https://placeimg.com/360/360/tech',
+        icon: 'carbon:code',
+        poster: 'http://placeimg.com/640/480/tech',
+        contact: [
+            {
+                name: 'Devansh Parapalli',
+                phone: '1234567890'
+            }
+        ],
+        is_active: true
+    }
+]
