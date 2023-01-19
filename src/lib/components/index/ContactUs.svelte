b1744da7c47f550fd180e5744455d3e41379312c
Devansh Parapalli <parapallidev@gmail.com>
2023-01-19 17:07:17 +0530

    contact-us form lmao

--- src/lib/components/index/ContactUs.svelte
+++ src/lib/components/index/ContactUs.svelte
@@ -1,1 +1,90 @@
-<!-- Another swiper component, maybe 2 or 3 or howmany ever -->
+<script>
+
+	import { enhance } from "$app/forms";
+    let submit = false;
+</script>
+<div class="md:h-full h-5/6  w-5/6 flex flex-col md:flex-row justify-center items-center m-auto pt-24 ">
+    <div class="section md:w-1/3 w-5/6 hidden md:block h-5/6 bg-black/20 py-8 md:py-0 rounded-lg border border-white">
+        <form on:submit={() => {
+            submit = true;
+        }} 
+        class="p-10" method="POST" use:enhance action="/forms/contact_us">
+            <div class="relative z-0 mb-6 w-full group">
+				<input
+					type="text"
+					name="name"
+					id="name"
+					class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+					placeholder=" "
+					required
+				/>
+				<label
+					for="name"
+					class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-200 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+					>Name</label
+				>
+			</div>
+            <div class="relative z-0 mb-6 w-full group">
+				<input
+					type="email"
+					name="email"
+					id="email"
+					class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+					placeholder=" "
+					required
+				/>
+				<label
+					for="email"
+					class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-200 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+					>Email</label
+				>
+			</div>
+            <div class="relative z-0 mb-6 w-full group">
+				<textarea
+					name="message"
+					id="message"
+					class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+					placeholder=" "
+					required
+				/>
+				<label
+					for="message"
+					class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-200 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+					>Message</label
+				>
+			</div>
+            <button disabled={submit} class="btn btn-outline btn-block" type="submit">Submit</button>
+        </form>
+        
+    </div>
+    <div id="center-console" class="section md:w-1/3 w-5/6 h-5/6  flex flex-col items-center justify-between py-8 md:py-0 rounded-lg border border-black bg-white/20">
+    <h3 class="text-2xl md:text-6xl pt-0 md:pt-10">Contact Us</h3>
+    <div class="contact-info px-4 pt-4 flex flex-col gap-8 items-start">
+        <div class="card mx-auto bg-black/50 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title">M. Parmesan</h2>
+                <a href="tel:+918793150182" class="inline-flex items-center gap-2"><iconify-icon icon="mdi:phone-outline"/> 8793150182</a>
+                <a href="mailto:parapallidev@gmail.com" class="inline-flex items-center gap-2"><iconify-icon icon="mdi:email-outline"/>parapallidev@gmail.com</a>
+              </div>
+          </div>
+          <div class="card mx-auto bg-black/50 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">P. Parmesan</h2>
+              <a href="tel:+918793150182" class="inline-flex items-center gap-2"><iconify-icon icon="mdi:phone-outline"/> 8793150182</a>
+              <a href="mailto:parapallidev@gmail.com" class="inline-flex items-center gap-2"><iconify-icon icon="mdi:email-outline"/>parapallidev@gmail.com</a>
+            </div>
+          </div>
+    </div>
+    <div class="contact-links"></div>
+    </div>
+    <div class="section md:w-1/3 w-5/6 h-5/6 bg-black/20 py-8 md:py-0 rounded-lg border border-white">
+        <iframe title="Google Map for GCOEN" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.525563997084!2d79.05785451517583!3d21.051661192371412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4be5e617378eb%3A0x682e0bc768d22adb!2sGovernment%20Engineering%20College%2CNagpur!5e0!3m2!1sen!2sin!4v1674120640996!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="h-full w-full z-[220]"></iframe>
+    </div>
+</div>
+
+<style>
+    .section {
+        backdrop-filter: blur(2px);
+    }
+</style>
+
--- src/lib/components/index/Sponsors.svelte
+++ src/lib/components/index/Sponsors.svelte
@@ -1,32 +1,46 @@
-<div class="h-full w-full flex flex-col pt-10 lg:flex-row items-center justify-evenly gap-10">
-    <div class="text-content w-3/4 text-justify text-lg md:text-2xl leading-6 flex flex-col items-center justify-center bg-clip-text text-transparent bg-gradient-to-tr from-pink-200 via-fuchsia-200 to-sky-200 font-semibold"><div class="max-w-xl">Over the past years Adhyaaya has had the privilege to have hosted a number of sponsors which provided a very entertaining experience to our visitors as well as the brand. The crowd engagement, media exposure, sampling and brand building opportunities offered at Adhyaaya are unparalleled. The fest has always been a platform for several brands to engage in unique crowd engaging activations which has proved to deliver a strong sponsor take back value.</div>
-    <a href="/sponsors" class="da-button text-white mr-auto mt-auto md:mt-4 md:ml-auto md:mr-0 text-2xl md:text-4xl relative p-4 md:p-8 max-w-lg text-center bg-gradient-to-r from-pink-500 via-fuchsia-500 to-sky-500 rounded-xl transition-all duration-300 hover:scale-105 active:scale-95 active:opacity-90 inline-flex items-center justify-center gap-3"><span>View Sponsors</span> <iconify-icon icon="mdi:arrow-right" /></a>
+<div
+	class="h-full w-3/4 mx-auto lg:w-full flex flex-col pt-8 lg:flex-row items-center justify-evenly gap-28 md:gap-10"
+>
+	<div
+		class="text-content h-full w-full lg:w-2/3 text-justify text-lg md:text-2xl leading-6 flex flex-col items-center justify-center bg-clip-text text-transparent bg-gradient-to-tr from-pink-200 via-fuchsia-200 to-sky-200 font-semibold"
+	>
+		<div class="max-w-xl">
+			Over the past years Adhyaaya has had the privilege to have hosted a number of sponsors which
+			provided a very entertaining experience to our visitors as well as the brand. The crowd
+			engagement, media exposure, sampling and brand building opportunities offered at Adhyaaya are
+			unparalleled. The fest has always been a platform for several brands to engage in unique crowd
+			engaging activations which has proved to deliver a strong sponsor take back value. 
+		</div>
+        <a
+				href="/sponsors"
+				class="da-button text-white mb-10 md: mt-10 mx-auto text-2xl md:text-4xl relative p-4 md:p-8 max-w-lg text-center bg-gradient-to-r from-pink-500 via-fuchsia-500 to-sky-500 rounded-xl transition-all duration-300 hover:scale-105 active:scale-95 active:opacity-90 inline-flex items-center justify-center gap-3"
+				><span>View Sponsors</span> <iconify-icon icon="mdi:arrow-right" /></a
+			>
+	</div>
+	<div class="lg:w-1/3" />
 </div>
-    <div class="w-1/4"></div>
-</div>
-
 
 <style>
-        @keyframes move-da-button {
-        0%, 100% {
-            background-position: left center;
-        }
-        50% {
-            background-position: right center;
-        }
-        
-    }
-        
-    .da-button {
-        background-size: 200% auto;
-        animation: move-da-button 10s ease infinite;
-    }
+	@keyframes move-da-button {
+		0%,
+		100% {
+			background-position: left center;
+		}
+		50% {
+			background-position: right center;
+		}
+	}
 
-    .da-button::before {
-        background-size: 200% auto;
-        animation: move-da-button 10s ease infinite;
-        z-index: -1;
-        content: '';
-        @apply absolute h-full w-full blur-2xl p-8 max-w-lg text-center bg-gradient-to-r from-pink-500 via-fuchsia-500 to-sky-500 rounded-xl transition-all duration-300 scale-105;
-    }
+	.da-button {
+		background-size: 200% auto;
+		animation: move-da-button 10s ease infinite;
+	}
+
+	.da-button::before {
+		background-size: 200% auto;
+		animation: move-da-button 10s ease infinite;
+		z-index: -1;
+		content: '';
+		@apply absolute h-full w-full blur-2xl p-8 max-w-lg text-center bg-gradient-to-r from-pink-500 via-fuchsia-500 to-sky-500 rounded-xl transition-all duration-300 scale-105;
+	}
 </style>
