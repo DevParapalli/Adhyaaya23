107ca69c981db92efa57c6320d81ab4758666666
Devansh Parapalli <parapallidev@gmail.com>
2023-02-09 20:08:31 +0530

    corrected color for website

--- src/lib/components/index/Footer.svelte
+++ src/lib/components/index/Footer.svelte
@@ -9,7 +9,7 @@
 
 <div class="md:h-full pt-32
  w-11/12  m-auto flex flex-col lg:flex-row items-stretch justify-evenly pb-4">
-    <div class="h-full lg:w-1/3 bg-purple-900/10 order-2 lg:order-1 p-4">
+    <div class="h-full lg:w-1/3 bg-purple-900/20 order-2 lg:order-1 p-4">
     <!-- Links Here, 2 headings -->
     <div class="flex flex-col items-stretch justify-evenly p-4 h-1/2">
         <div class="heading text-2xl btn btn-ghost btn-lg transition-none hover:scale-100 active:scale-100 hover:bg-opacity-0 cursor-auto border-0 border-b-2 rounded-b-none border-white hover:text-white">Sitemap</div>
@@ -34,7 +34,7 @@
         <div class="h-5/6 w-full bg-black/50">
             <iframe title="Google Map for GCOEN" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.525563997084!2d79.05785451517583!3d21.051661192371412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4be5e617378eb%3A0x682e0bc768d22adb!2sGovernment%20Engineering%20College%2CNagpur!5e0!3m2!1sen!2sin!4v1674120640996!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="h-full w-full z-[220]"></iframe>
         </div>
-        <div class="h-1/6 w-full bg-white/20 flex flex-row items-center justify-evenly py-8 text-white">
+        <div class="h-1/6 w-full bg-purple-900/20 flex flex-row items-center justify-evenly py-8 text-white">
         <button class="email h-8 w-8 lg:h-12 lg:w-12 bg-sky-500 rounded-full text-2xl lg:text-4xl inline-flex items-center justify-center hover:scale-105 active:scale-95 active:opacity-95 transition-all duration-300 ease-in-out"><iconify-icon icon="mdi:email-outline"/></button>
         <button class="linkedin h-8 w-8 lg:h-12 lg:w-12 bg-blue-500 rounded-full text-2xl lg:text-4xl inline-flex items-center justify-center hover:scale-105 active:scale-95 active:opacity-95 transition-all duration-300 ease-in-out"><iconify-icon icon="mdi:linkedin"/></button>
         <button class="instagram h-8 w-8 lg:h-12 lg:w-12 bg-purple-500 rounded-full text-2xl lg:text-4xl inline-flex items-center justify-center hover:scale-105 active:scale-95 active:opacity-95 transition-all duration-300 ease-in-out"><iconify-icon icon="mdi:instagram"/></button>
