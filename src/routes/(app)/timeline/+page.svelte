ac1d46fb868cfb36a88cc60ba198caaa2cc6b824
Devansh Parapalli <parapallidev@gmail.com>
2023-02-13 22:13:27 +0530

    reformatted timeline for efficient data entry

--- src/routes/(app)/timeline/+page.svelte
+++ src/routes/(app)/timeline/+page.svelte
@@ -1,135 +1,36 @@
-<div class="pt-28">
-    <h2 class="text-3xl font-bold text-center">Timeline</h2>
-  </div>
-  <div class="flex flex-col md:flex-row">
-    <div class="w-full md:w-1/3 p-4">
-      <div class="text-center font-bold text-xl">Day 1</div>
-      <div class="flex flex-col">
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">1</div>
-          <div class="text-center">9:00 AM</div>
-          <div class="text-center">Meeting with team</div>
-          <div class="text-center">In-person</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
+<script lang="ts">
+	enum Mode {
+		ONLINE = 'Online',
+		OFFLINE = 'Offline'
+	}
 
-        <!-- Repeat the above block 10 more times to get 12 slots in total -->
-      </div>
-    </div>
-    <!-- Repeat the above block for Day 2 and Day 3 -->
-    <div class="w-full md:w-1/3 p-4">
-      <div class="text-center font-bold text-xl">Day 1</div>
-      <div class="flex flex-col">
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">1</div>
-          <div class="text-center">9:00 AM</div>
-          <div class="text-center">Meeting with team</div>
-          <div class="text-center">In-person</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        
-        <!-- Repeat the above block 10 more times to get 12 slots in total -->
-      </div>
-    </div>
-    <div class="w-full md:w-1/3 p-4">
-      <div class="text-center font-bold text-xl">Day 1</div>
-      <div class="flex flex-col">
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">1</div>
-          <div class="text-center">9:00 AM</div>
-          <div class="text-center">Meeting with team</div>
-          <div class="text-center">In-person</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        <div class="w-full p-2 border border-gray-400">
-          <div class="text-center font-bold">2</div>
-          <div class="text-center">10:00 AM</div>
-          <div class="text-center">Client call</div>
-          <div class="text-center">Online</div>
-        </div>
-        
-        <!-- Repeat the above block 10 more times to get 12 slots in total -->
-      </div>
-    </div>
-  </div>
+	let TIMELINE: { time: string; name: string; mode: Mode }[][] = [
+		// Day 1
+		[],
+		// Day 2
+		[],
+		// Day 3
+		[]
+	];
+</script>
+
+<div class="pt-28">
+	<h2 class="text-3xl font-bold text-center">Timeline</h2>
+</div>
+<div class="flex flex-col md:flex-row">
+	{#each TIMELINE as DAY, i}
+		<div class="w-full md:w-1/3 p-4">
+			<div class="text-center font-bold text-xl">Day {i + 1}</div>
+			<div class="flex flex-col">
+				{#each DAY as item, i}
+					<div class="w-full p-2 border border-gray-400">
+						<div class="text-center font-bold">{i + 1}</div>
+						<div class="text-center">{item.time}</div>
+						<div class="text-center">{item.name}</div>
+						<div class="text-center">{item.mode}</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/each}
+</div>
