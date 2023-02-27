e9a062496281defedae1e93c619d813fd021b166
Devansh Parapalli <parapallidev@gmail.com>
2023-02-27 22:34:22 +0530

    removed pseudoevents

--- src/routes/_test/data/+page.svelte
+++ src/routes/_test/data/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import {EVENTS} from '$lib/data/events';
+
 </script>
 
 <div class="pt-28 pb-10 px-10 flex flex-wrap gap-4">
-    {#each EVENTS as e}
+    {#each EVENTS.filter(e => !e.id.includes('::')) as e}
         <a href="/_test/data/{e.id}" class="mb-10 rounded-lg bg-white/10 p-4">
             <h2 class="text-2xl font-bold">{e.name}</h2>
         </a>
